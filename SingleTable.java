f93444a4e1a94249185eb3311280db922ea7f536
socratest3 <socratest3@nycstudents.net>
2025-05-04 00:38:35 -0400

    Update SingleTable.java

--- SingleTable.java
+++ SingleTable.java
@@ -1,1 +1,41 @@
+public class SingleTable 
+{
+    /**
+     * Returns the number of seats at this table. The value is always greater than
+     * or equal to 4.
+     */
+    private int seats;
+    private double viewQuality;
+    private int height;
+    public SingleTable(int s , double v, int h)
+  {
+        seats = s;
+        viewQuality = v;
+        height = h;
+    }
 
+    public int getNumSeats() 
+    {
+       return seats; 
+    }
+
+    /** Returns the height of this table in centimeters. */
+    public int getHeight() 
+    {
+      return height; 
+    }
+
+    /** Returns the quality of the view from this table. */
+    public double getViewQuality() 
+    {
+        return viewQuality;
+    }
+
+    /** Sets the quality of the view from this table to value . */
+    public void setViewQuality(double value) 
+    {
+       viewQuality = value; 
+    }
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
