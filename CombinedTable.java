6467d26d996901345f145d02d850c8a071d7610d
socratest3 <socratest3@nycstudents.net>
2025-05-04 00:35:44 -0400

    Update CombinedTable.java

--- CombinedTable.java
+++ CombinedTable.java
@@ -1,1 +1,22 @@
+public class CombinedTable
+{
+private SingleTable one;
+private SingleTable two;
 
+public CombinedTable(SingleTable o, SingleTable t)
+  {
+    one = o;
+    two = t;
+  }
+public boolean canSeat(int num)
+  {
+    return one.getNumSeats() + two.getNumSeats() - 2 >= num;
+  }
+
+public double getDesirability()
+  {
+    double average  = (one.getViewQuality() + two.getViewQuality()) / 2;
+    if(one.getHeight() == two.getHeight()) return average;
+    return average - 10;
+ }
+}
